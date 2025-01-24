21cf4dad906aec434f31b481c86ed9e29069d0d8
Dastan <117820555+dastanchiik@users.noreply.github.com>
2025-01-24 15:11:51 +0600

    Update GmailApi.java

--- src/main/java/com/example/foremail/api/GmailApi.java
+++ src/main/java/com/example/foremail/api/GmailApi.java
@@ -5,7 +5,6 @@ import com.example.foremail.gmail.GmailSender;
 import lombok.RequiredArgsConstructor;
 
 import org.springframework.web.bind.annotation.GetMapping;
-
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
